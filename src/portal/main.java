9358fb5769f41df7fb531c9815ac64e83b178212
wildangerm <wildanger.m@gmail.com>
2016-03-18 20:35:40 +0100

    jo

--- src/portal/main.java
+++ src/portal/main.java
@@ -3,12 +3,8 @@ package portal;
 public class main {
 	
 	public static void main(String[] args){
-<<<<<<< HEAD
 		System.out.println("Ez a wBranch");
 		String hello = "Hreo";
-=======
-		System.out.println("Ez a master");
->>>>>>> refs/remotes/origin/master
 	}
 	
 }
