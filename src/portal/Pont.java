35b0b166ec4256fa5a410fcf18f433f0fb5b9048
BotMan <baumann.istvan94@gmail.com>
2016-03-25 15:21:51 +0100

    Szakadék véglegesítése, Pont osztály létrehozása

--- src/portal/Pont.java
+++ src/portal/Pont.java
@@ -9,14 +9,26 @@ public class Pont {
 		this.y=y;
 		System.out.println("<Pont::konstruktor");
 	}
+	public void setX(int x){
+		System.out.println(">Pont::setX");
 		this.x=x;
+		System.out.println("<Pont::setX");
 	}
+	public void setY(int y){
+		System.out.println(">Pont::setY");
 		this.x=y;
+		System.out.println("<Pont::setY");
 	}
+	public int getX(){
+		System.out.println(">Pont::getX");
 		
+		System.out.println("<Pont::getX");
 		return x;
 	}
+	public int getY(){
+		System.out.println(">Pont::getY");
 		
+		System.out.println("<Pont::getY");
 		return y;
 	}
 
--- src/portal/Szakadek.java
+++ src/portal/Szakadek.java
@@ -3,15 +3,24 @@ package portal;
 public class Szakadek extends Ososztaly {
 	
 	//Konstruktor
-	public Szakadek() {}
+	public Szakadek(int x, int y) {
+		System.out.println(">Szakadek::konstruktor");
+		this.position.add(new Pont(x,y));
+		
+		System.out.println("<Szakadek::konstruktor");
+	}
 	
 	//Doboz megsemmisítése, ha beleesett
 	void destroy(Doboz d) throws Throwable{
+		System.out.println(">Szakadek::destroy");
 		d.destroy();
+		System.out.println("<Szakadek::destroy");
 	}
 	
 	//Ezredes megsemmisítése, ha beleesett
-	void kill(Ezredes e){
+	void kill(Ezredes e) throws Throwable{
+		System.out.println(">Szakadek::kill");
 		e.fallAndDie();
+		System.out.println("<Szakadek::kill");
 	}
 }
