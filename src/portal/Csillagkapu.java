d68b7d639f5fa9e4abc74c66183b364fcf1621a5
BotMan <baumann.istvan94@gmail.com>
2016-03-28 19:20:57 +0200

    csillagkapu entered fv-t leszámítva

--- src/portal/Csillagkapu.java
+++ src/portal/Csillagkapu.java
@@ -12,16 +12,24 @@ public class Csillagkapu {
 	 */
 	public Csillagkapu() {
 		feregjarat = false;
+		blue = null;
+		yellow = null;
 	}
 	
 	// Beállítja a kék privát SpecFal értékét a kapottra
 	void setBlue(SpecFal s){
 		blue = s;
+		if(yellow != null){
+			this.makeFeregjarat();
+		}
 	}
 	
 	// Beállítja a sárga privát SpecFal értékét a kapottra
 	void setYellow(SpecFal s){
 		yellow = s;
+		if(blue != null){
+			this.makeFeregjarat();
+		}
 	}
 	
 	//Keletkezik egy féregjárat, azaz a féregjárat értékét igazra állítjuk
