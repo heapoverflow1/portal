5b8167b79034ec5264b3b3952b18a2685cf63962
BJakab <jakab2008@gmail.com>
2016-04-01 23:37:16 +0200

    Merge remote-tracking branch 'refs/remotes/origin/szkeleton' into JakabAg2
    # Conflicts:
    #	bin/portal/Jatekter.class
    #	bin/portal/Ososztaly.class
    #	src/portal/Ezredes.java
    #	src/portal/Jatekter.java
    #	src/portal/Ososztaly.java

--- src/portal/Csillagkapu.java
+++ src/portal/Csillagkapu.java
@@ -5,64 +5,42 @@ public class Csillagkapu implements Ertesit{
 	SpecFal blue;
 	SpecFal yellow;
 	
-	//!TODO - mikor lesz false-ra allitva?
-	boolean feregjarat;
-	
+	boolean feregjarat;	
 	
 	/* Konstruktor
 	 * beallitja a feregjaratot false-ra. Alapertelmezes.
 	 */
 	public Csillagkapu(int x, int y) {
-		
 		feregjarat = false;
 		blue = null;
 		yellow = null;
 	}
 	
 	// Beallitja a kek privat SpecFal erteket a kapottra
-	void setBlue(SpecFal s){
-		
-		System.out.println(">Csillagkapu::setBlue(SpecFal)");
-		
-		blue = s;
-		if(yellow != null){
-			this.makeFeregjarat();
-		}
-		
+	void setBlue(SpecFal s){		
+		System.out.println(">Csillagkapu::setBlue(SpecFal)");		
+		//Ha van sárga létrehozva, féregjárat létrehozása.
+		//Külön hívást nem igényel.
 		System.out.println("<Csillagkapu::setBlue(SpecFal)");
 	}
 	
 	// Beallitja a sarga privat SpecFal erteket a kapottra
 	void setYellow(SpecFal s){
-		
 		System.out.println(">Csillagkapu::setYellow(SpecFal)");
-		
-		yellow = s;
-		if(blue != null){
-			this.makeFeregjarat();
-		}
-		
+		//Ha van kék létrehozva, féregjárat létrehozása.
+		//Külön hívást nem igényel.
 		System.out.println("<Csillagkapu::setYellow(SpecFal)");
 	}
 	
 	//Keletkezik egy feregjarat, azaz a feregjarat erteket igazra allitjuk
 	void makeFeregjarat(){
-		
-		System.out.println(">Csillagkapu::makeFeregjarat()");
-		feregjarat = true;
-		System.out.println("<Csillagkapu::makeFeregjarat()");
+		//Belsõ mûködéshez kell, a szkeletonban szerepe nincs.
 	}
 	
-	//!TODO
 	// ez visszaadhatna true/false erteket es akkor azt nezzük, hogy mit adott vissza
-	void entered(){
-		
-		System.out.println(">Csillagkapu::entered()");
-		
-		//!TODO
-		
-		System.out.println(">Csillagkapu::entered()");
-		
+	void entered(){		
+		System.out.println(">Csillagkapu::entered()");		
+		System.out.println("<Csillagkapu::entered()");		
 	}
 	
 	/* Ha az EZREDES a sarga kapuba lep bele, akkor a kek kapuban jelenik meg,
@@ -70,22 +48,9 @@ public class Csillagkapu implements Ertesit{
 	 * Azaz az adott Pontot adjuk vissza neki. 
 	 */
 	public Pont ertesit(Pont innenlep) {		
-		System.out.println(">Csillagkapu::ertesit(Pont)");		
-		
-		if(yellow.position.compareTo(innenlep)){
-			
-			System.out.println("<Csillagkapu::ertesit(Pont)");
-			return blue.position;
-		}
-		else if(blue.position.compareTo(innenlep)){
-			
-			System.out.println("<Csillagkapu::ertesit(Pont)");
-			return yellow.position;
-		}else{
-			
-			System.out.println("<Csillagkapu::ertesit(Pont)");
-			return null;
-		}
-
+		System.out.println(">Csillagkapu::ertesit(Pont)");
+		//A belsõ mûködés része, hogy milyen Pontot adunk vissza, külön kérdést nem igényel.
+		System.out.println("<Csillagkapu::ertesit(Pont)");
+		return null;
 	}
 }
