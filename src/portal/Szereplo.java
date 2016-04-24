e898979025936563ae9ded4074ef199813aa75a5
wildangerm <wildanger.m@gmail.com>
2016-04-24 23:06:03 +0200

    Jatekos, szereplo kesz, NO jatek vege
    csak a JATEK VEGE nincs meg megcscinalva valahol

--- src/portal/Szereplo.java
+++ src/portal/Szereplo.java
@@ -3,29 +3,58 @@ package portal;
 public abstract class Szereplo extends Ososztaly {
 	Irany irany;	
 	
+	/* Konstruktor
+	 * A SZEREPLO a jatek elejen jobbra all
+	 * A SZEREPLO inicializalasa x, y koordinatakkal
+	 */
 	public Szereplo(int x, int y){
 		position = new Pont(x, y);
 		irany = Irany.JOBBRA;
 	}
 	
-	public void move (Irany i){		
-			
+	/* JATEKOS mozgatasa irany iranyba
+	 * irany valtozoja beallitasa
+	 * Megprobal lepni a JATEKOS egyet az adott iranyba,
+	 * majd megkeri a JATEKTER-et, hogy hivja meg az adott
+	 * pozicion levo dolognak az ERTESIT fuggvenyet, ami visszater egy
+	 * adott pozicioval a targytol fuggoen ( ha FAL, akkor a regi pozicio, azaz nem leptunk,
+	 * ha MERLEG, akkor az uj pozicio, tehat leptunk)
+	 */
+	public void move (Irany i){
+		
+		this.irany = i;
+		
+		Pont ujhely = position;
+		ujhely.move(irany);
+		position = Jatek.palya.checkfield(position, ujhely);
 	}
+	
+	/*  SZEREPLO leesik, ezaltal meghal, vege a jateknak 
+	 */
 	
 	public void fallAndDie(){
 		
+		try {
+			this.finalize();
+			
+		} catch (Throwable e) {			
+			e.printStackTrace();
+		}		
 		
 	}
 
-	@Override
+	/*  Egymasra ralephetnek a SZEREPLOk, ezert az ertesit fuggveny
+	 *  visszaadja az adott jatekos poziciojat 
+	 */
 	public Pont ertesit(Pont innenlep, Szereplo sz) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return position;
 	}
 
-	@Override
-	public void ertesit_shoot(Tolteny t) {
-		// TODO Auto-generated method stub
+	/*  Atmehet a SZEREPLOn a TOLTENY ezert nem csinalunk semmit a TOLTENNYEL
+	 *  Feluldefinialva a REPLIKATORban
+	 */
+	public void ertesit_shoot(Tolteny t) {		
 		
 	}
 	
