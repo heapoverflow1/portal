9be4f4f2f4d8ad4910d27e9dca1d89ea9d64187d
wildangerm <wildanger.m@gmail.com>
2016-04-24 23:43:40 +0200

    jatekosok, replikator hozzaadasa

--- src/portal/Jatek.java
+++ src/portal/Jatek.java
@@ -5,5 +5,23 @@ public class Jatek {
 	
 	public static void main(String[] args){
 		palya = new Jatekter();
+		
+		/*  EZREDESt hozunk letre (a SARGA szin meghatarozza)
+		 *  es hozzadjuk a palyalistahoz
+		 */
+		palya.add(new Jatekos(1,1, Szin.SARGA));
+		
+		/*  JAFFAt hozunk letre (a ZOLD szin meghatarozza)
+		 *  es hozzaadjuk a palyalistahoz
+		 */
+		palya.add(new Jatekos(2,2, Szin.ZOLD));
+		
+		/*  REPLIKATORt hozunk letre
+		 *  es hozzaadjuk a palyalistahoz
+		 */
+		palya.add(new Replikator(3, 3));
+		
+		//!TODO - JATEKTER VÉGTELEN CIKLUSAT MEGIRNI
+		
 	}
 }
