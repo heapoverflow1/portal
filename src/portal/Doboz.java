a1bed2f2336e995311398762bcbb99b411f363fd
BJakab <jakab2008@gmail.com>
2016-04-25 01:30:26 +0200

    Dobozzal valami nem stimmel
    javítva

--- src/portal/Doboz.java
+++ src/portal/Doboz.java
@@ -13,68 +13,32 @@ public class Doboz extends Ososztaly{
 	
 	//Doboz felemelese
 	public void Lift(){
-<<<<<<< HEAD
 		isLifted = true;
-=======
-		
-		
-		isLifted = true;
-		
->>>>>>> refs/remotes/origin/master
 	}
 	
 	//Doboz letevese
 	public void Drop(){
-<<<<<<< HEAD
 		isLifted = false;
-=======
-		
-		
-		isLifted = false;
-		
->>>>>>> refs/remotes/origin/master
+
 	}
 	
 	//Doboz helyenek allitasa
 	public void setPosition(Pont uj){
-<<<<<<< HEAD
 		if (isLifted)
 			position = uj;
+	}
+
+	void destroy() throws Throwable{
+		this.finalize();		
 	}
 
 	//Az ertesit interfesz megvalositasa miatt szukseges, hogy fel lehessen venni a jatekter objektumai koze. A doboz sajat poziciojaval ter vissza.
 	//Egy kesobbi bovites alkalmaval kaphat a metodus nagyobb szerepet, ha pl. be szeretnenk allitani, hogy legyenek olyan dobozok, amikkel nem lehet egy mezore lepni.
 
-	public Pont ertesit(Pont regi, Szereplo sz){
-		return position;
-=======
-		
-		
-		if (isLifted)
-			position = uj;
-		
->>>>>>> refs/remotes/origin/master
-	}
-	
-
-	
-	//Doboz megszunik
-<<<<<<< HEAD
-	void destroy() throws Throwable{		
-		this.finalize();
-=======
-	void destroy() throws Throwable{
-		
-		
-		this.finalize();
-		
-	}
-
 	@Override
 	public Pont ertesit(Pont innenlep, Szereplo sz) {
 		// TODO Auto-generated method stub
 		return position;
->>>>>>> refs/remotes/origin/master
 	}
 
 	public void ertesit_shoot(Tolteny t) {
