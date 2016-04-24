717a2c26ce85edd88c16c52c44b0c97f09c845fc
benceszabo1209 <benceszabo1209@gmail.com>
2016-04-24 23:10:51 +0200

    replicator fallanddie

--- src/portal/Szakadek.java
+++ src/portal/Szakadek.java
@@ -1,40 +1,42 @@
 package portal;
 
 public class Szakadek extends Ososztaly {
-	
-	/* Konstruktor
-	 * A SZAKADEK inicializalasa x, y koordinatakkal
+
+	/*
+	 * Konstruktor A SZAKADEK inicializalasa x, y koordinatakkal
 	 */
-	public Szakadek(int x, int y) {		
-		this.position = new Pont(x,y);		
+	public Szakadek(int x, int y) {
+		this.position = new Pont(x, y);
 	}
-	
-	//Doboz megsemmisitése, ha beleesett
-	void destroy(Doboz d) throws Throwable{
-		
+
+	// Doboz megsemmisitése, ha beleesett
+	void destroy(Doboz d) throws Throwable {
+
 		System.out.println(">Szakadek::destroy(Doboz)");
 		d.destroy();
 		System.out.println("<Szakadek::destroy(Doboz)");
 	}
-	
-	//Ezredes megsemmisitése, ha beleesett
-	void kill(Jatekos e) throws Throwable{
-		
-		System.out.println(">Szakadek::kill(Ezredes)");
-		e.fallAndDie();
-		System.out.println("<Szakadek::kill(Ezredes)");
+
+	// Ezredes megsemmisitése, ha beleesett
+	void kill(Szereplo e) throws Throwable {
+		if (e.getClass().getSimpleName() == "Replikator") {
+			e.fallAndDie();
+			this.finalize();
+		} else {
+			e.fallAndDie();
+		}
+		System.out.println("<Szakadek::kill(Szereplo)");
 	}
-	
-	
-	//!TODO - leírás + lentebbi !TODO
-	public Pont ertesit(Pont regi, Szereplo sz){
-		
+
+	// !TODO - leírás + lentebbi !TODO
+	public Pont ertesit(Pont regi, Szereplo sz) {
+
 		System.out.println(">Szakadek::ertesit(Pont)");
-		
-		//TODO!!!
-		//NINCS KEZELVE AZ EZREDES KINYÍRÁS!
-		//this.kill(Ezredes);
-		
+
+		// TODO!!!
+		// NINCS KEZELVE AZ EZREDES KINYÍRÁS!
+		// this.kill(Ezredes);
+
 		System.out.println("<Szakadek::ertesit(Pont)");
 		return position;
 	}
@@ -42,6 +44,6 @@ public class Szakadek extends Ososztaly {
 	@Override
 	public void ertesit_shoot(Tolteny t) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
