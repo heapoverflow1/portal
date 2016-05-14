9805b8bf773b20b2caea8eee84c3de700ddb2b74
nbence <nadaiben@yahoo.co.uk>
2016-05-14 23:23:52 +0200

    view class üres fv-ek

--- src/portal/View.java
+++ src/portal/View.java
@@ -6,4 +6,71 @@ public class View {
 		jatekter=jatek;
 	}
 	public void Init(){}
+	
+	public void ZPMadded(ZPM z) {		
+		
+	}
+	
+	public void ZPMcollected(ZPM z){
+		
+	}
+	
+	public void DobozMoved(Doboz d) {
+		
+	}
+	
+	public void DobozDestroyed(Doboz d){
+		
+	}
+	
+	public void AjtoOpened(Ajto a){
+		
+	}
+	
+	public void AjtoClosed(Ajto a){
+		
+	}
+	
+	public void CSKCreated(Csillagkapu cs){
+		
+		
+	}
+	
+	public void CSKClosed(Csillagkapu cs){
+		
+		
+	}
+	
+	public void MerlegSulyOK(Merleg m){
+		
+	}
+	
+	public void MerlegSulyNOTOK(Merleg m){
+		
+	}
+	
+	public void SzakadekDied(Szakadek sz){
+		
+	}
+	
+	public void SzereploMoved(Szereplo sz){
+		
+	}
+	
+	public void JatekosDied(Jatekos j){
+		
+	
+	}
+	
+	public void ReplikatorDied(Replikator r){
+		
+		
+	}
+	
+	public void ReplikatorFallAndDie(Replikator r, Szakadek sz){
+		
+	}
+	
+	
+	
 }
